b6b5b40a1a22a04f0bd7b7139fa3c1d788c22820
TheWebartist <thewebartist@outlook.com>
2022-12-14 02:31:11 +0100

    Add required changes to demo hosted page to have a valid communication between page and WebView2 control A an Win UI demo project to demonstrates communication

---
+++ demos/ExternalNotify.Demos.Wasdk/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿namespace ExternalNotify.Demos.Wasdk
+{
+    using ExternalNotify.Demos.Wasdk.Views;
+    using Microsoft.UI;
+    using Microsoft.UI.Xaml;
+    using Windows.UI;
+    using Windows.UI.ViewManagement;
+
+    /// <summary>
+    /// Demo application class
+    /// </summary>
+    public partial class App : Application
+    {
+        #region Fields
+
+        private Window startWindow;
+
+        #endregion Fields
+
+        #region Constructor
+
+        /// <summary>
+        /// Entry point
+        /// </summary>
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        #endregion Constructor
+
+        #region Application events
+
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            startWindow = new WebViewWindow();
+            startWindow.Activate();
+        }
+
+        #endregion Application events
+    }
+}
---
+++ demos/ExternalNotify.Demos.Wasdk/Views/WebViewWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿namespace ExternalNotify.Demos.Wasdk.Views
+{
+    using ExternalNotify.Demos.Uwp.Models;
+    using Microsoft.UI;
+    using Microsoft.UI.Windowing;
+    using Microsoft.UI.Xaml;
+    using Microsoft.UI.Xaml.Controls;
+    using Microsoft.Web.WebView2.Core;
+    using System;
+    using System.Collections.ObjectModel;
+    using Windows.ApplicationModel;
+    using WinRT.Interop;
+
+    /// <summary>
+    /// Demo main page
+    /// </summary>
+    public sealed partial class WebViewWindow : Window
+    {
+        #region Constructor
+
+        public WebViewWindow()
+        {
+            InitializeComponent();
+            DemoWebView2.WebMessageReceived += OnDemoWebView2WebMessageReceived;
+            TryAdaptTitleBar();
+        }
+
+        #endregion Constructor
+
+        #region Properties
+
+        public readonly ObservableCollection<ExternalNotifyDemoMessage> ReceivedMessages = new ObservableCollection<ExternalNotifyDemoMessage>();
+
+        public string Version
+            => GetFormatedVersion();
+
+        public Uri WebViewSourceUri
+            => new Uri("https://externalnotify.netlify.app");
+
+        #endregion Properties
+
+        #region Private methods
+
+        private string GetFormatedVersion()
+        {
+            var appVersion = Package.Current.Id.Version;
+            return $"{appVersion.Major}.{appVersion.Minor}.{appVersion.Revision}";
+        }
+
+        private void TryAdaptTitleBar()
+        {
+            WindowId wndId = Win32Interop.GetWindowIdFromWindow(WindowNative.GetWindowHandle(this));
+            var appWindow = AppWindow.GetFromWindowId(wndId);
+            if (AppWindowTitleBar.IsCustomizationSupported())
+            {
+                var titleBar = appWindow.TitleBar;
+                // Hide default title bar.
+                titleBar.ExtendsContentIntoTitleBar = true;
+
+                // Set active window colors
+                titleBar.ForegroundColor = Colors.White;
+                titleBar.BackgroundColor = Colors.Transparent;
+                titleBar.ButtonForegroundColor = Colors.White;
+                titleBar.ButtonBackgroundColor = Colors.SlateBlue;
+                titleBar.ButtonHoverForegroundColor = Colors.Gainsboro;
+                titleBar.ButtonHoverBackgroundColor = Colors.DarkSlateBlue;
+                titleBar.ButtonPressedForegroundColor = Colors.Silver;
+                titleBar.ButtonPressedBackgroundColor = Colors.DarkSlateBlue;
+
+                // Set inactive window colors
+                titleBar.InactiveForegroundColor = Colors.Gainsboro;
+                titleBar.InactiveBackgroundColor = Colors.LightSteelBlue;
+                titleBar.ButtonInactiveForegroundColor = Colors.Gainsboro;
+                titleBar.ButtonInactiveBackgroundColor = Colors.LightSteelBlue;
+            }
+            else
+            {
+                // Title bar customization using these APIs is currently
+                // supported only on Windows 11. In other cases, hide
+                // the custom title bar element.
+                AppTitleBar.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        #region Event handlers
+
+        private void OnDemoWebView2WebMessageReceived(WebView2 sender, CoreWebView2WebMessageReceivedEventArgs args)
+        {
+            if (!string.IsNullOrWhiteSpace(args.WebMessageAsJson))
+            {
+                ReceivedMessages.Add(new ExternalNotifyDemoMessage { MessageType = "JSON", Value = args.WebMessageAsJson });
+            }
+            else
+            {
+                ReceivedMessages.Add(new ExternalNotifyDemoMessage { MessageType = "Raw", Value = args.TryGetWebMessageAsString() });
+            }
+        }
+
+        #endregion Event handlers
+
+        #endregion Private methods
+    }
+}
